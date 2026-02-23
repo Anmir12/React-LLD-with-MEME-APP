fafc0e155f5a6a43fda9bf7ca69025a8eab2973d
Anmir12 <skannu30@gmail.com>
2026-02-23 23:03:29 +0530

    fetch memes api data and display it in ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import Loader from "./components/Loader";
 
 function App() {
+  const [memesData, setMemesData] = useState([]);
+  // const [isLoading,setIsLoading] =useState(false);
+
+  useEffect(() => {
+    fetchMemesData();
+  }, []);
+ 
+ const fetchMemesData = async () => {
+    try {
+      // setIsLoading(true);
+      const data = await fetch("https://meme-api.com/gimme/20");
+      const json = await data.json();
+      const {memes} =json;
+      console.log(json);
+      console.log(memes);
+      setMemesData(memes);
+      // setIsLoading(false)
+    } catch (error) {
+      if (error) {
+        return new Error("Error while making an api call");
+      }
+      // setIsLoading(false)
+    }
+  };
   return (
-    <>
-   <Loader/>
-    </>
+    <div className="memes-container">
+      {memesData.map((meme, idx) => {
+        return <div className="flex h-100 w-100 border border-black " key={meme?.url}>
+          <p>{meme?.author}</p>
+          <p>{meme?.title}</p>
+        </div>;
+      })}
+    </div>
   );
 }
 
